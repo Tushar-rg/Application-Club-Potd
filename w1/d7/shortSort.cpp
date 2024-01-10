84f9a9c9964967ffad3566879a2724bcef1db52a
Tushar-rg <77859244+Tushar-rg@users.noreply.github.com>
2024-01-10 16:33:46 +0530

    day 7

--- w1/d7/shortSort.cpp
+++ w1/d7/shortSort.cpp
@@ -1,8 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    string goal = "abc";
-    
-    return 0;
+string goal = "abc";
+ 
+void solve() {
+	string s;
+	cin >> s;
+	int count = 0;
+	for (int i = 0; i < 3; i++) {
+		count += (s[i] != goal[i]);
+	}
+	cout << (count <= 2 ? "YES\n" : "NO\n");
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n; cin >> n; for (int i = 1; i <= n; i++) {solve();}
 }
