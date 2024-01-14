8dc7b66a48d43325d358003c3ee2be05a26e8834
Tushar-rg <77859244+Tushar-rg@users.noreply.github.com>
2024-01-14 20:00:53 +0530

    day15

---
+++ w2/d14/stringTask.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	string str,temp="";
+	int left=0;
+ 	cin>>str;
+ 	for(int i=0;i<str.length();i++){
+ 		if( str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U' || str[i]=='Y' ||
+		 	str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='y')
+ 			continue;
+		else if(str[i]>=65 && str[i]<=90)
+			str[i]+=32;
+		temp+=str[i];
+	}
+	for(int i=0;i<temp.length();i++){
+		cout<<'.'<<temp[i];
+	}
+}
