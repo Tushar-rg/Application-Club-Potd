90f88a14aed22879e60b093ef3b6578176ce8b4b
Tushar-rg <77859244+Tushar-rg@users.noreply.github.com>
2024-01-15 23:47:24 +0530

    day15

--- w3/d15/smlpalindrome.cpp
+++ w3/d15/smlpalindrome.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string makeSmallestPalindrome(string s) {
-        int front = 0, back = s.size()-1;
+        int front=0, back=s.size()-1;
         while(front<=s.size()/2){
             s[front]=min(s[front], s[back]);
             s[back]=s[front];
