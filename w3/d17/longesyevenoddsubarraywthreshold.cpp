c9a2d0b81377dd86c3980899534db439798099bc
Tushar-rg <77859244+Tushar-rg@users.noreply.github.com>
2024-01-19 23:17:10 +0530

    day18

--- w3/d17/longesyevenoddsubarraywthreshold.cpp
+++ w3/d17/longesyevenoddsubarraywthreshold.cpp
@@ -1,18 +1,18 @@
 int longestAlternatingSubarray(vector<int>& nums, int threshold) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==0 && nums[i]<=threshold){
+    int ans=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]%2==0 && nums[i]<=threshold){
+            i++;
+            int f=1;
+            while(i<nums.size()){
+                if((nums[i-1]%2)==(nums[i]%2) || nums[i]>threshold)
+                    break;
+                f++;
                 i++;
-                int f=1;
-                while(i<nums.size()){
-                    if((nums[i-1]%2)==(nums[i]%2) || nums[i]>threshold)
-                        break;
-                    f++;
-                    i++;
-                }
-                i--;
-                ans = ans>f ? ans : f;
             }
+            i--;
+            ans = ans>f ? ans : f;
         }
-        return ans;
     }
+    return ans;
+}
