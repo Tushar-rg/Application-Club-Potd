53e60584973899b5fc4dc255a94293dee9192250
Tushar-rg <77859244+Tushar-rg@users.noreply.github.com>
2024-01-22 00:20:58 +0530

    day22

--- w4/d22/sortParity.cpp
+++ w4/d22/sortParity.cpp
@@ -4,9 +4,8 @@ public:
         vector<int> even;
         vector<int> odd;
         vector<int> ans;
-        int count=0;
         for(int i=0;i<nums.size();i++)
-            (nums[i]%2==0) ? even.push_back(nums[i]) : odd.push_back(nums[i]);
+            nums[i]%2==0 ? even.push_back(nums[i]) : odd.push_back(nums[i]);
             
         for(int i=0;i<even.size();i++){
             ans.push_back(even[i]);
