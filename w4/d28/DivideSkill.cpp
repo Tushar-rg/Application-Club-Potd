6bffc9be18c49a8876339467d54b7c98fcba08a7
Tushar-rg <77859244+Tushar-rg@users.noreply.github.com>
2024-01-26 23:51:02 +0530

    day26

---
+++ w4/d28/DivideSkill.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        sort(skill.begin(),skill.end());
+        int n = skill.size();
+        int chem = skill[0] + skill[n-1];
+        long sum = 0;
+        for(int i=0;i<n;i++)
+            cout<<skill[i]<<" ";
+        for(int i=0;i<n/2;i++){
+            if(skill[i]+skill[n-i-1]!=chem) return -1;
+            else sum = sum+skill[i]*skill[n-i-1];
+        }
+        cout<<sum;
+        return sum;
+    }
+};
