c1e17bf86d35b0b3375730f8cbbdf869256957b6
Tushar-rg <77859244+Tushar-rg@users.noreply.github.com>
2024-01-27 23:37:29 +0530

    day27

--- w4/d27/DivideSkill.cpp
+++ w4/d27/DivideSkill.cpp
@@ -4,13 +4,10 @@ public:
         sort(skill.begin(),skill.end());
         int n = skill.size(), chem = skill[0] + skill[n-1];
         long sum = 0;
-        for(int i=0;i<n;i++)
-            cout<<skill[i]<<" ";
         for(int i=0;i<n/2;i++){
             if(skill[i]+skill[n-i-1]!=chem) return -1;
             else sum = sum+skill[i]*skill[n-i-1];
         }
-        cout<<sum;
         return sum;
     }
 };
